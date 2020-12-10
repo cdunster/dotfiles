ca765da421a9f028e95a035ec4dad14901516b74
cdunster <cdunster@users.noreply.github.com>
2020-12-10 17:38:19 +0100

    Add comments to fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
+# Enable VI keybindings in the fish shell.
 function fish_user_key_bindings
     fish_vi_key_bindings
 end
 
+# Alias for dotfiles bare git repo. Used as a backup for my dotfiles.
 alias config='/usr/bin/git --git-dir=/home/callum/.cfg/ --work-tree=/home/callum'
 
+# Enable the starship cross-shell prompt.
 starship init fish | source
