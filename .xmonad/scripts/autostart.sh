666eac54b2cdae1dcfd1f6b0945fbbda87825e34
cdunster <cdunster@users.noreply.github.com>
2021-01-06 13:41:21 +0100

    Add unclutter to autostart.

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -44,6 +44,7 @@ blueberry-tray &
 picom --config $HOME/.xmonad/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
+unclutter &
 
 #starting user applications at boot time
 #nitrogen --restore &
