a4798600a27d77352ef3c92379ce0e1b9b39bce2
cdunster <cdunster@users.noreply.github.com>
2021-01-18 10:32:51 +0100

    Stop projectile from automatically tracking projects.

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-clone-default-directory "~/repos/"))
+ '(magit-clone-default-directory "~/repos/")
+ '(projectile-track-known-projects-automatically nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
