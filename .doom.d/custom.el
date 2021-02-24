ad8830504e2f7084f49913d9888baa90546a5a1e
cdunster <cdunster@users.noreply.github.com>
2021-02-24 09:13:01 +0100

    Set Emacs' time locale to UNIX.

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-clone-default-directory "~/repos/")
- '(projectile-track-known-projects-automatically nil))
+ '(projectile-track-known-projects-automatically nil)
+ '(system-time-locale "C"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
