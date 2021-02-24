d1c72d69d465f2916c18dd5def64c1dba3682542
cdunster <cdunster@users.noreply.github.com>
2021-02-24 09:11:12 +0100

    Set EDITOR to Vim. Used for things such as git.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,8 @@ function fish_user_key_bindings
     fish_vi_key_bindings
 end
 
+# Set the default terminal editor to be vim.
+set -gx EDITOR vim
+
 # Enable the starship cross-shell prompt.
 starship init fish | source
