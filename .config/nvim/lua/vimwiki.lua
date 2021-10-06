0570198f4fff2c89ff3bab9d5de3fb974eedd841
Callum Dunster <cdunster@users.noreply.github.com>
2021-10-06 17:37:31 +0200

    Add WhichKey descriptions for vimwiki.

--- .config/nvim/lua/vimwiki.lua
+++ .config/nvim/lua/vimwiki.lua
@@ -5,3 +5,30 @@ vim.g.vimwiki_list = {{
     links_space_char = '_'
 }}
 
+
+local wk = require("which-key")
+
+--Vimwiki key bindings for <leader>+w.
+wk.register({
+    name = "+vimwiki",
+    ["w"] = "Open default wiki index file",
+    ["t"] = "Open default wiki index file in new tab",
+    ["s"] = "List available wiki index files",
+    ["i"] = "Open default diary index file",
+--     ["d"] = "Delete this wiki file",
+--     ["r"] = "Rename this wiki file",
+--     ["h"] = "Generate HTML from wiki",
+--     ["n"] = "Go to wiki entry",
+}, { prefix = "<leader>w"})
+
+--Vimwiki diary key bindings for <leader>+w+<leader>.
+wk.register({
+    name = "+diary",
+    ["i"] = "Generate diary links",
+    ["w"] = "Open today's entry",
+    ["t"] = "Open today's entry in new tab",
+    ["m"] = "Open tomorrow's entry",
+    ["y"] = "Open yesterday's entry",
+}, { prefix = "<leader>w<leader>"})
+
+--TODO Add descriptions for when in vimwiki.
