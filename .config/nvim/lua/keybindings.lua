ad31c7dcb71b6713643df26eee5e6316ed47e3f0
Callum Dunster <cdunster@users.noreply.github.com>
2021-10-06 16:43:32 +0200

    Improve WhichKey descriptions.

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -37,98 +37,98 @@ vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
 
 --Navigate errors.
 wk.register({
-    ["[e"] = { "<Plug>(coc-diagnostic-prev)", "previous-error" },
-    ["]e"] = { "<Plug>(coc-diagnostic-next)", "next-error" },
+    ["[e"] = { "<Plug>(coc-diagnostic-prev)", "Go to previous error" },
+    ["]e"] = { "<Plug>(coc-diagnostic-next)", "Go to next error" },
 })
 
 --Code navigation. With g prefix.
 wk.register({
-    ["d"] = { "<Plug>(coc-definition)", "goto-definition" },
-    ["y"] = { "<Plug>(coc-type-definition)", "goto-type-definition" },
-    ["i"] = { "<Plug>(coc-implementation)", "goto-implementation" },
-    ["r"] = { "<Plug>(coc-references)", "list-references" },
+    ["d"] = { "<Plug>(coc-definition)", "Go to definition" },
+    ["y"] = { "<Plug>(coc-type-definition)", "Go to type definition" },
+    ["i"] = { "<Plug>(coc-implementation)", "Go to implementation" },
+    ["r"] = { "<Plug>(coc-references)", "List references" },
 }, { prefix = "g" })
 
 
 --Map function and class text objects
 --NOTE: Requires 'textDocument.documentSymbol' support from the language server.
 wk.register({
-    ["if"] = { "<Plug>(coc-funcobj-i)", "inside function" },
-    ["af"] = { "<Plug>(coc-funcobj-a)", "around function" },
-    ["ic"] = { "<Plug>(coc-classobj-i)", "inside class" },
-    ["ac"] = { "<Plug>(coc-classobj-a)", "around class" },
+    ["if"] = { "<Plug>(coc-funcobj-i)", "Inside function" },
+    ["af"] = { "<Plug>(coc-funcobj-a)", "Around function" },
+    ["ic"] = { "<Plug>(coc-classobj-i)", "Inside class" },
+    ["ac"] = { "<Plug>(coc-classobj-a)", "Around class" },
 }, { mode = "x" })
 
 wk.register({
-    ["if"] = { "<Plug>(coc-funcobj-i)", "inside function" },
-    ["af"] = { "<Plug>(coc-funcobj-a)", "around function" },
-    ["ic"] = { "<Plug>(coc-classobj-i)", "inside class" },
-    ["ac"] = { "<Plug>(coc-classobj-a)", "around class" },
+    ["if"] = { "<Plug>(coc-funcobj-i)", "Inside function" },
+    ["af"] = { "<Plug>(coc-funcobj-a)", "Around function" },
+    ["ic"] = { "<Plug>(coc-classobj-i)", "Inside class" },
+    ["ac"] = { "<Plug>(coc-classobj-a)", "Around class" },
 }, { mode = "o" })
 
 --Leader prefixed key bindings.
 wk.register({
-    ["q"] = { "<cmd>SClose | q<cr>", "quit-neovim" },
-    ["<leader>"] = { "<cmd>Files<cr>", "list-files-current-dir" },
-    ["/"] = { "<cmd>Rg<cr>", "fuzzy-search-current-dir" },
-    ["`"] = { "<C-^>", "alternative-file" },
-    [","] = { "<cmd>Buffers<cr>", "list-open-buffers" },
-    ["h"] = { "<cmd>SClose | cd ~<cr>", "close-session-go-home" },
+    ["q"] = { "<cmd>SClose | q<cr>", "Quit Neovim" },
+    ["<leader>"] = { "<cmd>Files<cr>", "Find a file in working dir" },
+    ["/"] = { "<cmd>Rg<cr>", "Search in files in working dir" },
+    ["`"] = { "<C-^>", "Go to alternative file" },
+    [","] = { "<cmd>Buffers<cr>", "List open buffers" },
+    ["h"] = { "<cmd>SClose | cd ~<cr>", "Close session and go home" },
 
     ["f"] = {
         name = "+files",
-        ["f"] = { "<cmd>Files ~<cr>", "find-file" },
-        ["s"] = { "<cmd>w<cr>", "save-current-buffer" },
-        ["S"] = { "<cmd>wa<cr>", "save-all-buffers" },
-        ["/"] = { "<cmd>BLines<cr>", "search-in-buffer" },
-        ["?"] = { "<cmd>Lines<cr>", "search-in-open-buffers" },
+        ["f"] = { "<cmd>Files ~<cr>", "Find a file" },
+        ["s"] = { "<cmd>w<cr>", "Save current buffer" },
+        ["S"] = { "<cmd>wa<cr>", "Save all buffers" },
+        ["/"] = { "<cmd>BLines<cr>", "Search in buffer" },
+        ["?"] = { "<cmd>Lines<cr>", "Search in open buffers" },
     },
 
     ["b"] = {
         name = "+buffer",
-        ["k"] = { "<cmd>bd<cr>", "kill-current-buffer" },
-        ["K"] = { "<cmd>bd!<cr>", "kill-current-buffer-no-save" },
+        ["k"] = { "<cmd>bd<cr>", "kill current buffer" },
+        ["K"] = { "<cmd>bd!<cr>", "kill current buffer without saving" },
     },
 
     ["d"] = {
         name = "+dotfiles",
-        ["g"] = { "<cmd>FloatermNew lazygit -w ~ -g ~/.cfg<cr>", "lazygit-dotfiles" },
-        ["f"] = { "<cmd>Files ~/.config<cr>", "find-dotfile" },
+        ["g"] = { "<cmd>FloatermNew lazygit -w ~ -g ~/.cfg<cr>", "Open lazygit in dotfiles dir" },
+        ["f"] = { "<cmd>Files ~/.config<cr>", "Find a dotfile" },
         ["n"] = {
             name = "+neovim",
-            ["f"] = { "<cmd>Files ~/.config/nvim<cr>", "find-nvim-dotfile" },
-            ["i"] = { "<cmd>e ~/.config/nvim/init.lua<cr>", "init" },
-            ["p"] = { "<cmd>e ~/.config/nvim/lua/plugins.lua<cr>", "plugins" },
+            ["f"] = { "<cmd>Files ~/.config/nvim<cr>", "Find a Neovim dotfile" },
+            ["i"] = { "<cmd>e ~/.config/nvim/init.lua<cr>", "Open Neovim init dotfile" },
+            ["p"] = { "<cmd>e ~/.config/nvim/lua/plugins.lua<cr>", "Open Neovim plugins dotfile" },
         },
     },
 
     ["g"] = {
         name = "+git",
-        ["g"] = { "<cmd>LazyGit<cr>", "lazygit-root" },
-        ["s"] = { "<cmd>Git<cr>", "status" },
-        ["b"] = { "<cmd>Git blame<cr>", "blame-this-file" },
-        ["d"] = { "<cmd>Gdiffsplit<cr>", "diff-all-changes" },
+        ["g"] = { "<cmd>LazyGit<cr>", "Open floating lazygit for working dir" },
+        ["s"] = { "<cmd>Git<cr>", "Git status" },
+        ["b"] = { "<cmd>Git blame<cr>", "Git blame this file" },
+        ["d"] = { "<cmd>Gdiffsplit<cr>", "Git diff all changes" },
         ["f"] = {
             name = "+file",
-            ["r"] = { "<cmd>GRename<cr>", "rename-file" },
-            ["m"] = { "<cmd>GMove<cr>", "move-file" },
-            ["d"] = { "<cmd>GDelete<cr>", "delete-file" },
+            ["r"] = { "<cmd>GRename<cr>", "Git rename file" },
+            ["m"] = { "<cmd>GMove<cr>", "Git move file" },
+            ["d"] = { "<cmd>GDelete<cr>", "Git delete file" },
         },
     },
 
     ["t"] = {
         name = "+terminal",
-        ["n"] = { "<cmd>terminal<cr>", "new-terminal" },
-        ["f"] = { "<cmd>FloatermNew<cr>", "new-terminal-floating" },
-        ["b"] = { "<cmd>FloatermNew --wintype=split --height=0.2<cr>", "new-terminal-bottom" },
+        ["n"] = { "<cmd>terminal<cr>", "Create new terminal buffer" },
+        ["f"] = { "<cmd>FloatermNew<cr>", "Create new floating terminal" },
+        ["b"] = { "<cmd>FloatermNew --wintype=split --height=0.2<cr>", "Create new terminal on the bottom" },
     },
 
     ["c"] = {
         name = "+code",
-        ["r"] = { "<Plug>(coc-rename)", "rename" },
-        ["a"] = { "<Plug>(coc-codeaction-line)", "code-actions" },
-        ["e"] = { "<cmd>CocList diagnostics<cr>", "list-errors" },
-        ["o"] = { "<cmd>CocList outline<cr>", "code-outline" },
-        ["s"] = { "<cmd>CocList -I symbols<cr>", "list-symbols" },
+        ["r"] = { "<Plug>(coc-rename)", "Rename token" },
+        ["a"] = { "<Plug>(coc-codeaction-line)", "List code actions" },
+        ["e"] = { "<cmd>CocList diagnostics<cr>", "List errors" },
+        ["o"] = { "<cmd>CocList outline<cr>", "List code outline" },
+        ["s"] = { "<cmd>CocList -I symbols<cr>", "List code symbols" },
     },
 }, { prefix = "<leader>" })
