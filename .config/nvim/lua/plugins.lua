9e1f42b520ab1de8b5a45cba04a2d7f17fca08e0
Callum Dunster <cdunster@users.noreply.github.com>
2021-10-06 19:44:57 +0200

    Replace ' with " in plugins.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,21 +2,21 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+  execute "packadd packer.nvim"
 end
 
 -- List the plugins to install.
-return require('packer').startup(function()
+return require("packer").startup(function()
     -- Packer can manage itself.
-    use 'wbthomason/packer.nvim'
+    use "wbthomason/packer.nvim"
     -- Floating terminal in Vim.
-    use 'voldikss/vim-floaterm'
+    use "voldikss/vim-floaterm"
     -- Code completion.
-    use {'neoclide/coc.nvim', branch = 'release'}
+    use { "neoclide/coc.nvim", branch = "release" }
     -- Which Key.
     use {
       "folke/which-key.nvim",
@@ -29,33 +29,33 @@ return require('packer').startup(function()
       end
     }
     -- Start Screen.
-    use 'mhinz/vim-startify'
+    use "mhinz/vim-startify"
     -- FZF fuzzy file searcher.
-    use 'junegunn/fzf'
-    use 'junegunn/fzf.vim'
+    use "junegunn/fzf"
+    use "junegunn/fzf.vim"
     -- Open LazyGit in a floating window.
-    use 'kdheepak/lazygit.nvim'
+    use "kdheepak/lazygit.nvim"
     -- Git commands.
-    use 'tpope/vim-fugitive'
+    use "tpope/vim-fugitive"
     -- Better Rust support. Uses Syntastic for highlighting.
-    use 'rust-lang/rust.vim'
+    use "rust-lang/rust.vim"
     -- Advanced syntax highlighting.
-    use 'vim-syntastic/syntastic'
+    use "vim-syntastic/syntastic"
     -- Dart language support.
-    use 'dart-lang/dart-vim-plugin'
+    use "dart-lang/dart-vim-plugin"
     -- Flutter support with commands.
-    use 'thosakwe/vim-flutter'
+    use "thosakwe/vim-flutter"
     -- VimWiki for note-taking.
-    use 'vimwiki/vimwiki'
+    use "vimwiki/vimwiki"
     -- For debugging programmes in Neovim.
-    use 'puremourning/vimspector'
+    use "puremourning/vimspector"
     -- Comment-out lines.
-    use 'tpope/vim-commentary'
+    use "tpope/vim-commentary"
     -- Gruvbox colour scheme.
-    use 'morhetz/gruvbox'
+    use "morhetz/gruvbox"
     -- Seoul 256 colour scheme.
-    use 'junegunn/seoul256.vim'
+    use "junegunn/seoul256.vim"
     -- PICO-8 support.
-    use 'Bakudankun/PICO-8.vim'
+    use "Bakudankun/PICO-8.vim"
 end)
 
